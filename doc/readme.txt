10dcfa378dd7fb817d16ff2af4e1941a6bc15333
neowyp <wypsmall@126.com>
2016-03-11 18:45:02 +0800

    add Notes

--- doc/readme.txt
+++ doc/readme.txt
@@ -1,6 +1,17 @@
 【2016-03-11】
 https://bitbucket.org/kubek2k/springockito/downloads
 
+1.王刚提供一种方式在spring的xml文件配置mock，使用的EasyMock实现的，扩展了spring标签，参考dubbo实现的
+
+2.我自己找一个开源包2012年就实现我的想法通过标签配置在spring的xml文件
+参见：D:\Code\Code_IntellijIdea\SSIBoss\src\test\resources\spring\test-orderAppCtx.xml
+<mockito:mock id="itemOpService" class="com.neo.order.service.IItemOpService"/>
+IItemOpService  只是一个接口类，没有时间类
+
+3.具体单元类参见：D:\Code\Code_IntellijIdea\SSIBoss\src\test\java\com\neo\test\order\TestOrderService.java
+
+
+
 【2016-03-10】
 计划学习Mockito
 研究将mock的对象注入到spring容器内，使用SpringJUnit4ClassRunner方式进行单元测试
--- src/test/java/com/neo/test/order/TestOrderService.java
+++ src/test/java/com/neo/test/order/TestOrderService.java
@@ -37,29 +37,33 @@ public class TestOrderService {
     @Autowired
     IOrderOpService orderOpService;
 
-    @Autowired
-    IOrderOpDao orderOpDao;
 
+//    已经通过spring的xml文件创建mock对象，所以这里不需要在手工mock了
 //    @Mock
 //    IItemOpService itemOpService;
 
     @Before
     public void initOperate() {
+//        不需要在初始化mock注解，如果有才初始化，目前当前类是没有注解mock所以可以注释
 //        MockitoAnnotations.initMocks(this);
+
+//        如果像mock方法，那么就需要先从spring容器取出，在定义mock方法的返回值
         IItemOpService itemOpService = (IItemOpService) ctx.getBean("itemOpService");
+
+//        在这里mock itemOpService的getItemById方法调用的返回值
         when(itemOpService.getItemById(20)).thenReturn(getItemEntity());
     }
 
     @Test
     public void testCreateOrder() {
-/*        OrderEntity orderEntity = new OrderEntity();
-        orderEntity.setOrderSN("D-" + System.currentTimeMillis());
-        orderEntity.setAmount(new BigDecimal("2.00"));
-        orderEntity.setExtend(orderEntity.toString());
-        orderOpDao.insert(orderEntity);*/
+//        事务配置是可以生效的，因为orderOpService是spring容器创建，所以通过声明式事务是生效了
+//        如果createOrder方法的入参不是20，那么when(itemOpService.getItemById(20)).thenReturn(getItemEntity());就不生效
         orderOpService.createOrder(20);
     }
 
+    /**
+     * @return mock item entity
+     */
     private static ItemEntity getItemEntity() {
         ItemEntity itemEntity = new ItemEntity();
         itemEntity.setId(123L);
